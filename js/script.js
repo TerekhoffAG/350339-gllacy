efdbfb9a44d29639b11d07b1a21d610b5bac3fe9
TerekhoffAG <38706246+TerekhoffAG@users.noreply.github.com>
2018-09-19 10:10:43 +0400

    Оживление интерактивных элементов в проекте Глейси

---
+++ js/script.js
@@ -0,0 +1,62 @@
+var link = document.querySelector(".feedback-btn");
+var popup = document.querySelector(".modal-feedback");
+var overlay = document.querySelector(".modal-overly");
+var close = popup.querySelector(".modal-close");
+var form = popup.querySelector("form");
+var username = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=email]");
+var text = popup.querySelector("[name=text]");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("username");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+link.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  overlay.classList.add("modal-overlay-show");
+  username.focus();
+});
+
+if (storage) {
+  username.value = storage;
+  email.focus();
+} else {
+  username.focus();
+};
+
+close.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+  overlay.classList.remove("modal-overlay-show");
+});
+
+form.addEventListener("submit", function(evt) {
+  if (!username.value || !email.value || !text.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("username", username.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+      overlay.classList.remove("modal-overlay-show");
+    }
+  }
+});
